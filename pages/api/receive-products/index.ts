178959307df75f278b2de658515ec398b404f58e
krune <lemuelcariazo01@gmail.com>
2023-05-20 16:00:07 +0800

    fix geo location issue

--- pages/api/receive-products/index.ts
+++ pages/api/receive-products/index.ts
@@ -29,55 +29,7 @@ const middleware =
     }
   };
 
-// const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-//   const {
-//     barcodeId,
-//     productName,
-//     quantity,
-//     sku,
-//     palletteLocation,
-//     dateReceived,
-//     expirationDate,
-//     poId,
-//     image,
-//   } = req.body;
 
-//   if (
-//     !barcodeId ||
-//     !productName ||
-//     !quantity ||
-//     !sku ||
-//     !palletteLocation ||
-//     !dateReceived ||
-//     !expirationDate ||
-//     !poId
-//   ) {
-//     return res.status(401).json({
-//       message: "Missing Details",
-//     });
-//   }
-
-//   try {
-//     const { newProduct, error } = await recieveProduct(
-//       barcodeId,
-//       productName,
-//       quantity,
-//       sku,
-//       palletteLocation,
-//       dateReceived,
-//       expirationDate,
-//       poId,
-//       image
-//     );
-
-//     return res.status(200).json({
-//       message: "Product Added",
-//       newProduct,
-//     });
-//   } catch (error) {
-//     return res.send(error);
-//   }
-// };
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   console.log("handler is working");
