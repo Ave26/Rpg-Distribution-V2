7c92fbc6c4d8839f5b16bda283205d14c147870a
Ave26 <129952906+Ave26@users.noreply.github.com>
2023-05-21 00:18:43 +0800

    Geolocation v7

--- pages/dashboard/delivery-management.tsx
+++ pages/dashboard/delivery-management.tsx
@@ -1,46 +1,106 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
-const MapDisplay = () => {
-  const [currentPosition, setCurrentPosition] = useState<{ lat: number | null; lng: number | null }>({
-    lat: null,
-    lng: null,
-  });
+type LocationEntry = {
+  latitude: number;
+  longitude: number;
+  timestamp: string;
+};
+
+const Geolocation = () => {
+  const [latitude, setLatitude] = useState<number | null>(null);
+  const [longitude, setLongitude] = useState<number | null>(null);
+  const [error, setError] = useState<string | null>(null);
+  const [locationLog, setLocationLog] = useState<LocationEntry[]>([]);
+  const [isTracking, setIsTracking] = useState(false);
 
   useEffect(() => {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(
-        position => {
-          const { latitude, longitude } = position.coords;
-          setCurrentPosition({ lat: latitude, lng: longitude });
+    if (isTracking && typeof window !== "undefined" && "geolocation" in window.navigator) {
+      const watchId = window.navigator.geolocation.watchPosition(
+        (position) => {
+          setLatitude(position.coords.latitude);
+          setLongitude(position.coords.longitude);
+          setLocationLog((prevLog) => [
+            ...prevLog,
+            {
+              latitude: position.coords.latitude,
+              longitude: position.coords.longitude,
+              timestamp: new Date().toISOString(),
+            },
+          ]);
         },
-        error => {
-          console.error('Error getting geolocation:', error);
+        (error) => {
+          setError(error.message);
         }
       );
-    } else {
-      console.error('Geolocation is not supported');
-    }
-  }, []);
 
-  const mapUrl = `https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d24859.092586071023!2d${currentPosition.lng || 0}!3d${currentPosition.lat || 0}!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMzDCsDM4JzQzLjAiTiA3N8KwMjknMTIuMiJF!5e0!3m2!1sen!2sus!4v1621055880878!5m2!1sen!2sus`;
+      return () => {
+        window.navigator.geolocation.clearWatch(watchId);
+      };
+    }
+  }, [isTracking]);
+
+  const startTracking = () => {
+    setIsTracking(true);
+  };
+
+  const stopTracking = () => {
+    setIsTracking(false);
+  };
+
+  const mapUrl = `https://maps.google.com/maps?q=${latitude || 0},${longitude || 0}&output=embed`;
 
   return (
-    <div style={{ height: '400px', width: '100%' }}>
-      <h1>Map Display</h1>
-      {currentPosition.lat !== null && currentPosition.lng !== null ? (
-        <iframe
-          title="Map"
-          width="100%"
-          height="100%"
-          frameBorder="0"
-          src={mapUrl}
-          allowFullScreen
-        ></iframe>
+    <div className="container mx-auto p-8">
+      <div className="mb-8">
+        <h1 className="text-3xl font-bold">Geolocation Map</h1>
+        <div className="h-64 mt-4">
+          {latitude && longitude ? (
+            <iframe
+              title="Map"
+              width="100%"
+              height="100%"
+              frameBorder="0"
+              src={mapUrl}
+              allowFullScreen
+            ></iframe>
+          ) : (
+            <p>Loading map...</p>
+          )}
+        </div>
+      </div>
+
+      {latitude && longitude && (
+        <p className="mb-4">
+          Latitude: {latitude}, Longitude: {longitude}
+        </p>
+      )}
+      {error && <p className="mb-4 text-red-500">Error: {error}</p>}
+      <h3 className="mb-2 text-lg font-bold">Location Log:</h3>
+      <ul className="mb-4">
+        {locationLog.map((location, index) => (
+          <li key={index} className="mb-1">
+            Latitude: {location.latitude}, Longitude: {location.longitude} - {location.timestamp}
+          </li>
+        ))}
+      </ul>
+
+      {!isTracking ? (
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          onClick={startTracking}
+        >
+          Start Tracking
+        </button>
       ) : (
-        <p>Loading map...</p>
+        <button
+          className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+          onClick={stopTracking}
+        >
+          Stop Tracking
+        </button>
       )}
     </div>
   );
 };
 
-export default MapDisplay;
+export default Geolocation;
