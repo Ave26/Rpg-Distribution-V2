bac81404cad2c2dbfae9d048135992a68b1abc0e
Ave26 <129952906+Ave26@users.noreply.github.com>
2023-05-21 13:16:03 +0800

    Revert to previous working version

--- pages/dashboard/delivery-management.tsx
+++ pages/dashboard/delivery-management.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState, useCallback } from "react";
+import { useEffect, useState } from "react";
 
 type LocationEntry = {
   latitude: number;
@@ -12,62 +12,21 @@ const Geolocation = () => {
   const [error, setError] = useState<string | null>(null);
   const [locationLog, setLocationLog] = useState<LocationEntry[]>([]);
   const [isTracking, setIsTracking] = useState(false);
-  const canvasRef = useRef<HTMLCanvasElement>(null);
-  const mapUrl = `https://maps.google.com/maps?q=${latitude ?? 0},${longitude ?? 0}&output=embed`;
-
-  const drawRoute = useCallback(() => {
-    if (canvasRef.current) {
-      const canvas = canvasRef.current;
-      const ctx = canvas.getContext("2d");
-
-      if (ctx) {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        ctx.strokeStyle = "red";
-        ctx.lineWidth = 2;
-
-        ctx.beginPath();
-
-        locationLog.forEach((location, index) => {
-          const x = (location.longitude - (longitude ?? 0)) * 1000 + canvas.width / 2;
-          const y = -((location.latitude - (latitude ?? 0)) * 1000) + canvas.height / 2;
-
-          if (index === 0) {
-            ctx.moveTo(x, y);
-          } else {
-            ctx.lineTo(x, y);
-          }
-        });
-
-        ctx.stroke();
-      }
-    }
-  }, [locationLog, latitude, longitude]);
 
   useEffect(() => {
     if (isTracking && typeof window !== "undefined" && "geolocation" in window.navigator) {
       const watchId = window.navigator.geolocation.watchPosition(
         (position) => {
-          const newLatitude = position.coords.latitude;
-          const newLongitude = position.coords.longitude;
-
-          if (
-            latitude !== newLatitude ||
-            longitude !== newLongitude ||
-            locationLog.length === 0
-          ) {
-            setLatitude(newLatitude);
-            setLongitude(newLongitude);
-            setLocationLog((prevLog) => [
-              ...prevLog,
-              {
-                latitude: newLatitude,
-                longitude: newLongitude,
-                timestamp: new Date().toISOString(),
-              },
-            ]);
-
-            drawRoute();
-          }
+          setLatitude(position.coords.latitude);
+          setLongitude(position.coords.longitude);
+          setLocationLog((prevLog) => [
+            ...prevLog,
+            {
+              latitude: position.coords.latitude,
+              longitude: position.coords.longitude,
+              timestamp: new Date().toISOString(),
+            },
+          ]);
         },
         (error) => {
           setError(error.message);
@@ -78,7 +37,7 @@ const Geolocation = () => {
         window.navigator.geolocation.clearWatch(watchId);
       };
     }
-  }, [isTracking, drawRoute, latitude, longitude, locationLog.length]);
+  }, [isTracking]);
 
   const startTracking = () => {
     setIsTracking(true);
@@ -88,37 +47,29 @@ const Geolocation = () => {
     setIsTracking(false);
   };
 
+  const mapUrl = `https://maps.google.com/maps?q=${latitude || 0},${longitude || 0}&output=embed`;
+
   return (
     <div className="container mx-auto p-8">
       <div className="mb-8">
         <h1 className="text-3xl font-bold">Geolocation Map</h1>
-        <div className="h-64 mt-4 relative">
-          {latitude != null && longitude != null ? (
-            <>
-              <iframe
-                title="Map"
-                width="100%"
-                height="100%"
-                frameBorder="0"
-                src={mapUrl}
-                allowFullScreen
-                style={{ zIndex: 0 }}
-              ></iframe>
-              <canvas
-                ref={canvasRef}
-                className="absolute top-0 left-0"
-                width="100%"
-                height="100%"
-                style={{ zIndex: 1 }}
-              ></canvas>
-            </>
+        <div className="h-64 mt-4">
+          {latitude && longitude ? (
+            <iframe
+              title="Map"
+              width="100%"
+              height="100%"
+              frameBorder="0"
+              src={mapUrl}
+              allowFullScreen
+            ></iframe>
           ) : (
             <p>Loading map...</p>
           )}
         </div>
       </div>
 
-      {latitude != null && longitude != null && (
+      {latitude && longitude && (
         <p className="mb-4">
           Latitude: {latitude}, Longitude: {longitude}
         </p>
