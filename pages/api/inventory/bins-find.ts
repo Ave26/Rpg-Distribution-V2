c7aac154e4b7a6070fba3698beccf5a3b2f6c4c0
krune <lemuelcariazo01@gmail.com>
2023-12-08 16:38:40 +0800

    added5

--- pages/api/inventory/bins-find.ts
+++ pages/api/inventory/bins-find.ts
@@ -23,74 +23,75 @@ export async function handler(
         let sortedBins = [];
 
         const categories = await prisma.categories.findMany({
-          include: {
-            racks: {
-              include: {
-                bins: {
-                  include: {
-                    _count: {
-                      select: {
-                        assignedProducts: true,
-                      },
-                    },
-                    assignedProducts: {
-                      select: {
-                        id: true,
-                        purchaseOrder: true,
-                        skuCode: true,
-                        dateReceive: true,
-                        expirationDate: true,
-                        quality: true,
-                        status: true,
-                        barcodeId: true,
-                        products: {
-                          select: {
-                            category: true,
-                            productName: true,
-                          },
-                        },
-                      },
-                    },
-                    racks: {
-                      include: {
-                        categories: true,
-                      },
-                    },
-                  },
-                },
-              },
-            },
-          },
+          include: { racks: { include: { bins: true } } },
+          // include: {
+          //   racks: {
+          //     include: {
+          //       bins: {
+          //         include: {
+          //           _count: {
+          //             select: {
+          //               assignedProducts: true,
+          //             },
+          //           },
+          //           assignedProducts: {
+          //             select: {
+          //               id: true,
+          //               purchaseOrder: true,
+          //               skuCode: true,
+          //               dateReceive: true,
+          //               expirationDate: true,
+          //               quality: true,
+          //               status: true,
+          //               barcodeId: true,
+          //               products: {
+          //                 select: {
+          //                   category: true,
+          //                   productName: true,
+          //                 },
+          //               },
+          //             },
+          //           },
+          //           racks: {
+          //             include: {
+          //               categories: true,
+          //             },
+          //           },
+          //         },
+          //       },
+          //     },
+          //   },
+          // },
         });
 
-        // Extract pagination parameters from query string
-        const page = parseInt(req.query.page as string) || 1;
-        const itemsPerPage = parseInt(req.query.itemsPerPage as string) || 4;
+        // // Extract pagination parameters from query string
+        // const page = parseInt(req.query.page as string) || 1;
+        // const itemsPerPage = parseInt(req.query.itemsPerPage as string) || 4;
 
-        // Calculate start and end indices for the current page
-        const startIndex = (page - 1) * itemsPerPage;
-        const endIndex = startIndex + itemsPerPage;
+        // // Calculate start and end indices for the current page
+        // const startIndex = (page - 1) * itemsPerPage;
+        // const endIndex = startIndex + itemsPerPage;
 
-        for (let category of categories) {
-          for (let rack of category.racks) {
-            const uniquePO = new Set();
-            for (let bin of rack.bins) {
-              uniquePO.add(bin?.assignedProducts);
-            }
-            // console.log(uniquePO);
-            sortedBins.push(...rack.bins);
-          }
-        }
+        // for (let category of categories) {
+        //   for (let rack of category.racks) {
+        //     const uniquePO = new Set();
+        //     for (let bin of rack.bins) {
+        //       uniquePO.add(bin?.assignedProducts);
+        //     }
+        //     // console.log(uniquePO);
+        //     sortedBins.push(...rack.bins);
+        //   }
+        // }
 
-        // Use slice to extract bins for the current page
-        const currentPageBins = sortedBins.slice(startIndex, endIndex);
+        // // Use slice to extract bins for the current page
+        // const currentPageBins = sortedBins.slice(startIndex, endIndex);
 
-        const totalBins = sortedBins.length;
+        // const totalBins = sortedBins.length;
 
-        // Calculate the last page
-        const lastPage = Math.ceil(totalBins / itemsPerPage);
+        // // Calculate the last page
+        // const lastPage = Math.ceil(totalBins / itemsPerPage);
 
-        return res.status(200).json({ bins: currentPageBins, lastPage });
+        return res.status(200).json(categories);
       } catch (error) {
         return console.log(error);
       }
