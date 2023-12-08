28cb8788ed6db7a1b8353e28b20b084866b65758
krune <lemuelcariazo01@gmail.com>
2023-12-08 16:16:11 +0800

    added v2

--- pages/dashboard/inventory-managements/index.tsx
+++ pages/dashboard/inventory-managements/index.tsx
@@ -8,9 +8,28 @@ export default function Inventory({ bins }: any) {
 }
 
 export async function getServerSideProps() {
-  // Fetch data from external API
-  const bins = await prisma.bins.findMany({});
-  console.log(bins);
-  // Pass data to the page via props
-  return { props: { bins: bins } };
+  try {
+    // Fetch data from external API
+    const bins = await prisma.bins.findMany({
+      include: {
+        assignedProducts: true,
+      },
+    });
+
+    // Convert Date objects to string
+    const serializedBins = bins.map((bin) => ({
+      ...bin,
+      assignedProducts: bin.assignedProducts.map((assignedProduct) => ({
+        ...assignedProduct,
+        dateReceive: assignedProduct?.dateReceive?.toISOString(),
+        expirationDate: assignedProduct?.expirationDate?.toISOString(),
+      })),
+    }));
+
+    // Pass data to the page via props
+    return { props: { bins: serializedBins } };
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    return { props: { bins: [] } }; // Handle the error gracefully
+  }
 }
