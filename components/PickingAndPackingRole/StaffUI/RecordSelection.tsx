b965ff1b3a2f3615d24ab26134e5f9a4554bac5e
lemuel cariazo <ldestacamento21@gmail.com>
2024-02-11 20:55:15 +0800

    Fix Picking and Packing Bugs

--- components/PickingAndPackingRole/StaffUI/RecordSelection.tsx
+++ components/PickingAndPackingRole/StaffUI/RecordSelection.tsx
@@ -12,11 +12,11 @@ export default function RecordSelection({ record }: TRecordSelectionProps) {
       <h1>Batch Number: {record.batchNumber}</h1>
       <h1>Purchase Order: {record.poId}</h1>
       {record.orderedProducts.map((orderedProduct) => (
-        <div className="flex h-fit flex-row justify-between gap-2">
-          <div
-            key={orderedProduct.id}
-            className="flex h-[7em] w-full items-start justify-start gap-2 overflow-y-auto border  border-black p-2"
-          >
+        <div
+          key={orderedProduct.id}
+          className="flex h-fit flex-row justify-between gap-2"
+        >
+          <div className="flex h-[7em] w-full items-start justify-start gap-2 overflow-y-auto border  border-black p-2">
             <OrderedProduct orderedProduct={orderedProduct} record={record} />
           </div>
         </div>
